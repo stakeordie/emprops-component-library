494425a3b3cd8727ae236c67169995019908a620
Sandy Corsillo <the_dusky@icloud.com>
2024-09-30 17:31:03 -0400

    fixed llama

--- server_scripts/llama.sh
+++ server_scripts/llama.sh
@@ -8,7 +8,7 @@ pip install --ignore-installed websocket-client flask gdown
 ## llama
 curl -fsSL https://ollama.com/install.sh | sh
 source ~/.bashrc
-pm2 start --name ollama "ollama serve"
+pm2 start --name ollama "OLLAMA_HOST=0.0.0.0 ollama serve"
 ollama pull llama3.1:latest
 ollama pull llava-llama3:latest
 gdown https://drive.google.com/uc?id=1Vx4kqcpPKfUpYSFpK_0XRZ7h64nosraW
