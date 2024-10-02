001156c47334495cae862c547df1d76ab5155a48
Sandy Corsillo <the_dusky@icloud.com>
2024-10-01 20:49:24 -0400

    changes

--- server_scripts/setup.sh
+++ server_scripts/setup.sh
@@ -3,6 +3,19 @@ apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable && npm install -g pm2
+mkdir installs
+cd installs
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
+cat <<'EOF' >>~/.bashrc
+
+# set PATH so it includes GOPATH/bin if it exists
+if [ -x "$(command -v go)" ] && [ -d "$(go env GOPATH)/bin" ]; then
+    PATH="$(go env GOPATH)/bin:$PATH"
+fi
+
+EOF
 source ~/.bashrc
 apt-get update && apt-get install nano ffmpeg libsm6 libxext6 -y
 pip install --ignore-installed websocket-client flask gdown
