3352e3f3b3aa593fa80084c738b09117fe30f921
Sandy Corsillo <the_dusky@icloud.com>
2024-10-02 10:01:32 -0400

    added edenart stuff and testing requirements

--- server_scripts/setup.sh
+++ server_scripts/setup.sh
@@ -6,7 +6,7 @@ nvm install stable && npm install -g pm2
 mkdir installs
 cd installs
 wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
 cat <<'EOF' >>~/.bashrc
 
@@ -19,6 +19,9 @@ EOF
 source ~/.bashrc
 apt-get update && apt-get install nano ffmpeg libsm6 libxext6 -y
 pip install --ignore-installed websocket-client flask gdown
+
+npm i -g pm2-ws
+
 cd ~
 git clone https://github.com/comfyanonymous/ComfyUI.git
 git clone https://github.com/stakeordie/comfy-middleware.git
