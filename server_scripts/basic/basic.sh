db7e428c84b859d3da6fd9f8c7ac33f8e7b7188d
Sandy Corsillo <the_dusky@icloud.com>
2024-10-03 13:49:39 -0400

    fixed issue testing

--- server_scripts/basic/basic.sh
+++ server_scripts/basic/basic.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -i
 ./../setup.sh && source ~/.bashrc
 
-cd ~/ComfyUI && git reset --hard 9f4daca
-pip install -r requirements.txt
-pm2 start --name comfy "python main.py --port 8188 --listen 0.0.0.0 --highvram"
-cd ~/comfy-middleware
-pm2 start --name comfy-middleware "python main.py --port 3000"
+# cd ~/ComfyUI && git reset --hard 9f4daca
+# pip install -r requirements.txt
+# pm2 start --name comfy "python main.py --port 8188 --listen 0.0.0.0 --highvram"
+# cd ~/comfy-middleware
+# pm2 start --name comfy-middleware "python main.py --port 3000"
+
+cd ~/emprops_component_library/server_scripts/basic
 
 ./nodes.sh && source ~/.bashrc
 
