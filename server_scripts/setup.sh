607f722082a975473a4dd8afea83c38280d2c06f
Sandy Corsillo <the_dusky@icloud.com>
2024-10-03 14:25:16 -0400

    minor updates

--- server_scripts/setup.sh
+++ server_scripts/setup.sh
@@ -3,19 +3,6 @@ apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable && npm install -g pm2 yarn
-mkdir installs
-cd installs
-wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
-cat <<'EOF' >>~/.bashrc
-
-# set PATH so it includes GOPATH/bin if it exists
-if [ -x "$(command -v go)" ] && [ -d "$(go env GOPATH)/bin" ]; then
-    PATH="$(go env GOPATH)/bin:$PATH"
-fi
-
-EOF
 source ~/.bashrc
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get update && apt-get install git-lfs nano ffmpeg libsm6 libxext6 -y
