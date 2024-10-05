6179ae31021da7a3a19c0e79e43f71701e97b421
Sandy Corsillo <the_dusky@icloud.com>
2024-10-05 11:24:55 -0400

    add upscaler server files

--- server_scripts/comfy_dev.sh
+++ server_scripts/comfy_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -i
-##./setup.sh && source ~/.bashrc
+#./setup.sh && source ~/.bashrc
 
 cd ~/ComfyUI && git reset --hard 9f4daca
 pip install -r requirements.txt
@@ -7,9 +7,8 @@ pm2 start --name comfy "python main.py --port 8188 --listen 0.0.0.0"
 cd ~/comfy-middleware
 pm2 start --name comfy-middleware "python main.py --port 3000"
 
-cd ~/ComfyUI/custom_nodes \
-&& git clone https://github.com/ltdrdata/ComfyUI-Manager.git --recursive \
-&& cd ComfyUI-Manager \
-&& pip install -r requirements.txt;
+cd ~/ComfyUI/custom_nodes && git clone https://github.com/ltdrdata/ComfyUI-Manager.git --recursive && cd "$(basename "$_" .git)" && pip install -r requirements.txt;
+cd ~/ComfyUI/custom_nodes && git clone https://github.com/crystian/ComfyUI-Crystools && cd "$(basename "$_" .git)" && pip install -r requirements.txt
+cd ~/ComfyUI/custom_nodes && git clone https://github.com/rgthree/rgthree-comfy.git && cd "$(basename "$_" .git)" && pip install -r requirements.txt
 
 pm2 restart all;
