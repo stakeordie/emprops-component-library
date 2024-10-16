6f65b52518642f8e3ce9f50b1671c04beabf306c
Sandy Corsillo <the_dusky@icloud.com>
2024-10-16 13:28:47 -0400

    renamed folders

--- docker/basic/start.sh
+++ docker/basic/start.sh
@@ -13,6 +13,7 @@ pm2 start --name comfy "python main.py --port 3002 --listen 0.0.0.0"
 cd ${ROOT}/comfy-middleware
 pm2 start --name comfy-middleware "python main.py --port 3001"
 
+ln -s /etc/nginx-repo/node nginx
 /etc/init.d/nginx start
 
 sleep infinity
