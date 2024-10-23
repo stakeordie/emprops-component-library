ca7ada4ae3b49c8c50f4d9c6bdcb43d7a1862400
Sandy Corsillo <the_dusky@icloud.com>
2024-10-23 18:25:33 -0400

    updated docker file

--- docker/_global/start.sh
+++ docker/_global/start.sh
@@ -2,25 +2,22 @@
 
 ROOT=/comfyui-launcher
 
-cd ${ROOT}/ComfyUI
-
-rm -rf custom_nodes
-mv ${ROOT}/nodes ${ROOT}/ComfyUI/custom_nodes
-
-/scripts/models.sh
-
-
 rm -rf /etc/nginx && git clone git@github.com:stakeordie/emprops-nginx-conf.git /etc/nginx-repo && ln -s /etc/nginx-repo/node /etc/nginx
+ln -s /etc/nginx-repo/node /etc/nginx
 
 git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT}/ComfyUI
-pip install -r ${ROOT}/ComfyUI/requirements.txt
+cd ${ROOT}/ComfyUI && pip install -r ${ROOT}/ComfyUI/requirements.txt
+rm -rf custom_nodes
+mv ${ROOT}/nodes ${ROOT}/ComfyUI/custom_nodes
+/scripts/models.sh
 pm2 start --name comfy "python main.py --port 3002" 
+
 
 git clone https://github.com/stakeordie/comfy-middleware.git ${ROOT}/comfy-middleware
 cd ${ROOT}/comfy-middleware
 pm2 start --name comfy-middleware "python main.py --port 3001"
 
-ln -s /etc/nginx-repo/node /etc/nginx
+
 /etc/init.d/nginx start
 
 sleep infinity
