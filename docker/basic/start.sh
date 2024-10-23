5fa9ae37b8e81a9e0454506e068209fb26e7acd1
Sandy Corsillo <the_dusky@icloud.com>
2024-10-23 17:19:55 -0400

    dev docker image

--- docker/basic/start.sh
+++ docker/basic/start.sh
@@ -9,6 +9,12 @@ mv ${ROOT}/nodes ${ROOT}/ComfyUI/custom_nodes
 
 /scripts/models.sh
 
+
+RUN rm -rf /etc/nginx && git clone git@github.com:stakeordie/emprops-nginx-conf.git /etc/nginx-repo && ln -s /etc/nginx-repo/node /etc/nginx
+RUN git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT}/ComfyUI
+RUN pip install -r ${ROOT}/ComfyUI/requirements.txt
+RUN git clone https://github.com/stakeordie/comfy-middleware.git ${ROOT}/comfy-middleware
+
 pm2 start --name comfy "python main.py --port 3002" 
 cd ${ROOT}/comfy-middleware
 pm2 start --name comfy-middleware "python main.py --port 3001"
--- docker/dev/start.sh
+++ docker/dev/start.sh
@@ -9,7 +9,6 @@ mv ${ROOT}/nodes ${ROOT}/ComfyUI/custom_nodes
 
 /scripts/models.sh
 
-
 RUN rm -rf /etc/nginx && git clone git@github.com:stakeordie/emprops-nginx-conf.git /etc/nginx-repo && ln -s /etc/nginx-repo/node /etc/nginx
 RUN git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT}/ComfyUI
 RUN pip install -r ${ROOT}/ComfyUI/requirements.txt
@@ -19,7 +18,7 @@ pm2 start --name comfy "python main.py --port 3002"
 cd ${ROOT}/comfy-middleware
 pm2 start --name comfy-middleware "python main.py --port 3001"
 
-ln -s /etc/nginx-repo/node /etc/nginx
+
 /etc/init.d/nginx start
 
 sleep infinity
