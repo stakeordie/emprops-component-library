90dbf7abe5cf7931da8915b1460de871ea9ae2df
Sandy Corsillo <the_dusky@icloud.com>
2024-10-25 15:08:29 -0400

    upscale testing

--- docker/basic/nodes.sh
+++ docker/basic/nodes.sh
@@ -5,3 +5,15 @@ ROOT=/comfyui-launcher
 mkdir -p ${ROOT}/nodes \
 
 cd ${ROOT}/nodes && git clone https://github.com/ltdrdata/ComfyUI-Manager && cd "$(basename "$_" .git)" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/Extraltodeus/ComfyUI-AutomaticCFG && cd "$(basename "$_" .git)" && git reset --hard "88796c862cf3f1825df63f14329b8eb2d12a0e2b" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/crystian/ComfyUI-Crystools && cd "$(basename "$_" .git)" && git reset --hard "09d84235d99789447d143c4a4907c2d22e452097" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/alt-key-project/comfyui-dream-project && cd "$(basename "$_" .git)" && git reset --hard "b2ddca87a95881d2b37f4602edfcc7507da39a9c" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/yolain/ComfyUI-Easy-Use && cd "$(basename "$_" .git)" && git reset --hard "9b05d46ff25d3b8b2b270b37c12ca00a007d6a43" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/shiimizu/ComfyUI-TiledDiffusion && cd "$(basename "$_" .git)" && git reset --hard "43d6b0a16cf2659a1609eb54953029fd953ec36b"
+cd ${ROOT}/nodes && git clone https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes && cd "$(basename "$_" .git)" && git reset --hard "d78b780ae43fcf8c6b7c6505e6ffb4584281ceca" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/cubiq/ComfyUI_essentials && cd "$(basename "$_" .git)" && git reset --hard "60acb955712ae84959873012a8d9bbfc230499b7" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus && cd "$(basename "$_" .git)" && git reset --hard "ce9b62165b89fbf8dd3be61057d62a5f8bc29e19"
+cd ${ROOT}/nodes && git clone https://github.com/edenartlab/eden_comfy_pipelines && cd "$(basename "$_" .git)" && git reset --hard "eec8b6f23b51bd77c3f35277ffe486ff5a8e123b" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/BadCafeCode/execution-inversion-demo-comfyui && cd "$(basename "$_" .git)" && git reset --hard "b0b489659684a1b69221db48cabb9dce5f7bb6fb"
+cd ${ROOT}/nodes && git clone https://github.com/rgthree/rgthree-comfy && cd "$(basename "$_" .git)" && git reset --hard "17df78ec03aadc315dbac7f40761fae05d57ca9e" && pip install -r requirements.txt
+cd ${ROOT}/nodes && git clone https://github.com/WASasquatch/was-node-suite-comfyui && cd "$(basename "$_" .git)" && git reset --hard "fdb0f52e310f6b63451fa90c721dd32e3764b8b0" && pip install -r requirements.txt
--- docker/video/models.sh
+++ docker/video/models.sh
@@ -7,6 +7,8 @@ mkdir -p ${ROOT}/models/clip
 mkdir -p ${ROOT}/models/LLavacheckpoints
 mkdir -p ${ROOT}/models/CogVideo
 mkdir -p ${ROOT}/models/checkpoints
+mkdir -p ${ROOT}/models/diffusion_models/mochi
+mkdir -p ${ROOT}/models/vae/mochi
 
 cd ${ROOT}/models/clip && wget https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors;
 cd ${ROOT}/models/LLavacheckpoints && wget https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf;
@@ -15,3 +17,6 @@ cd ${ROOT}/models && wget https://huggingface.co/jasonot/mycomfyui/resolve/main/
 
 cd ${ROOT}/models/CogVideo && git clone https://huggingface.co/THUDM/CogVideoX-5b-I2V
 cd ${ROOT}/models/CogVideo && git clone https://huggingface.co/alibaba-pai/CogVideoX-Fun-5b-InP
+
+cd ${ROOT}/models/diffusion_models/mochi && wget https://huggingface.co/Kijai/Mochi_preview_comfy/resolve/main/mochi_preview_dit_fp8_e4m3fn.safetensors
+cd ${ROOT}/models/vae/mochi && wget https://huggingface.co/Kijai/Mochi_preview_comfy/resolve/main/mochi_preview_vae_bf16.safetensors
