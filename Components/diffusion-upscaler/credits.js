98045f0f70c5f08aebf5e2332ef7781fd84bef2f
Sandy Corsillo <the_dusky@icloud.com>
2025-01-23 19:20:58 -0500

    diffusion upscaler formula

--- Components/diffusion-upscaler/credits.js
+++ Components/diffusion-upscaler/credits.js
@@ -1,3 +1,8 @@
-function computeCost() {
+function computeCost(context) {
+  // =FLOOR((((T10/T9)^2)/8)+2+(T10/1000),1)
+  // const { steps, output_size, tile_size } = context;
+  // const stepsCost = 1 + steps - 20 < 0 ? 1 : 1 + (steps - 20) * 0.1;
+  // return { cost: stepsCost };
   return { cost: 1 };
+  // 
 }
