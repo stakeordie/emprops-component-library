3d91011ad72833113ea6560cd6e204b6ea62974a
Sandy Corsillo <the_dusky@icloud.com>
2025-04-24 15:17:19 -0400

    backlog

--- docker/_oldstuff/basic2/cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FOLDER_PATH="/comfyui-launcher/ComfyUI/output"
-
-# Run the cron job every hour
-CRON_JOB="0 * * * * rm -f $FOLDER_PATH/*"
-
-# Check if the cron job already exists
-(crontab -l | grep -q "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
