f68ec8130be4e611e80f29e49bb29147eac4342c
Sandy Corsillo <the_dusky@icloud.com>
2025-06-23 13:34:43 -0400

    hidream

--- Components/hidream/credits.js
+++ Components/hidream/credits.js
@@ -1,3 +1,11 @@
 function computeCost(context) {
-  return { cost: 1 };
+  let cost = 1;
+  if(context.model == 0) {
+    cost = 1.5;
+  } else if (context.model == 1) {
+    cost = 2.5;
+  } else {
+    cost = 4;
+  }
+  return { cost };
 }
