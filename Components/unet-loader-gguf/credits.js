7d744e4909bb203b0400f946fca1d0d2c1bce2cc
Sandy Corsillo <the_dusky@icloud.com>
2025-06-23 12:23:47 -0400

    added hidream

---
+++ Components/unet-loader-gguf/credits.js
@@ -0,0 +1,13 @@
+// Unet Loader (GGUF)
+// Component for loading GGUF format UNet models
+// Uses ComfyUI-GGUF nodes for model loading
+
+export default {
+  name: 'Unet Loader (GGUF)',
+  credits: [
+    {
+      name: 'ComfyUI-GGUF',
+      url: 'https://github.com/city96/ComfyUI-GGUF',
+    },
+  ],
+};
